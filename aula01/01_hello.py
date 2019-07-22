34e1ff834763e9310a2848f73107c80dec6c9345
rotberto <53198088+rotberto@users.noreply.github.com>
2019-07-22 20:18:15 -0300

    Add files via upload

---
+++ aula01/01_hello.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+#shebang
+print ('hello world')
+
+
+#Variaveis
+
+mensagem = 'hello world'
+print (mensagem)
+
+print ('Python', 'Fundamentals', 10, 7, sep=':', end='\n\n')
---
+++ aula01/02_entrada.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+nome = input('Digite seu nome: ')
+
+if (nome == "Roberto"):
+    print ('Bem vindo {}'.format(nome))
+else:
+    print('Nome errado!')
+
+#Função print parametros sep e end
+#sep = separador dos itens
+#end = caracter de fim de linha
+#print ("Hello", 100, 10.5, sep=':', end='\n\n')
+
+# nome = input('Digite seu nome: ')
+
+# print('Seja bem vindo', nome)
